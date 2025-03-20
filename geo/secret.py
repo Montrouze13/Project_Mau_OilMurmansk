0129ebf6bdf843050c33438063fac75e2e06d599
Montrouze13 <montrouze@internet.ru>
2025-03-20 16:19:53 +0300

    Delete geo/secret.py

--- geo/secret.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Этот файл содержит параметры для авторизации на геопортале университета
-
-username = "student1"
-password = "ZfE-4wz-P3F-D2s"
-AUTH = (username, password)
-
-base_folder = 207 # Идентификатор базовой папки для пользователя student1
